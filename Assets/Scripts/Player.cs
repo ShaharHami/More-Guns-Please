024399b644824e3f52075463e52863b7f1a87ab2
shachar.hami <shachar.hami@galacoral.com>
2019-11-05 05:04:21 +0200

    Added joystick, flight and tilt controls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Joystick joystick;
+    [SerializeField] float speed = 1f;
+    [SerializeField] float tilt = 1f;
     void Start()
     {
         
@@ -11,6 +14,29 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        
+        Flight();
+        Tilt();
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            print(touch);
+        }
+    }
+
+    private void Flight()
+    {
+        transform.position += new Vector3(
+                    joystick.Direction.x * speed * Time.deltaTime,
+                    transform.position.y,
+                    joystick.Direction.y * speed * Time.deltaTime
+                );
+    }
+
+    private void Tilt()
+    {
+        Vector3 rot = transform.rotation.eulerAngles;
+        rot.z = joystick.Direction.x * -tilt * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(rot);
+        print(transform.rotation.eulerAngles);
     }
 }
