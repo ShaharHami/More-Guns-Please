a8db313d9c619f7e89485cd6f8bc170feecbc6de
shachar.hami <shachar.hami@galacoral.com>
2019-11-06 17:35:28 +0200

    Added Engine flames
    Added easing out function, Bounding to screen size and engine flames

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,57 +5,116 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Joystick joystick;
-    [SerializeField] ParticleSystem shots;
+    [SerializeField] ParticleSystem[] shots;
+    [SerializeField] ParticleSystem[] engines;
     [SerializeField] float speed = 1f;
+    [Range(0.0f, 1.0f)] [SerializeField] float speedEase = 1f;
+
+    private Vector3 _speed;
     [SerializeField] float tilt = 1f;
+    [Range(0.0f, 1.0f)] [SerializeField] float tiltEase = 1f;
+    private Vector3 _tilt;
+
+    private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
+
     void Start()
     {
-
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, Screen.height));
+        objectWidth = transform.GetComponent<MeshRenderer>().bounds.extents.x;
+        objectHeight = transform.GetComponent<MeshRenderer>().bounds.extents.z;
     }
 
     void FixedUpdate()
     {
-        Flight();
-        Roll();
+        ControlEngineFlameLength();
         if (Input.touchCount > 0)
         {
+            Flight();
+            Roll();
             Touch touch = Input.GetTouch(0);
             if (touch.phase == TouchPhase.Began)
             {
                 Shoot(true);
-            } 
+
+            }
             else if (touch.phase == TouchPhase.Ended)
             {
+                EaseOut();
                 Shoot(false);
             }
+        }
+        else if (Input.GetMouseButton(0)) // Mouse support for debugging purposes
+        {
+            Flight();
+            Roll();
+            Shoot(true);
+        }
+        else
+        {
+            EaseOut();
+            Shoot(false);
         }
     }
 
     private void Flight()
     {
-        transform.position += new Vector3(
-                    joystick.Direction.x * speed * Time.deltaTime,
+        _speed = new Vector3(
+                    joystick.Horizontal * speed * Time.deltaTime,
                     transform.position.y,
-                    joystick.Direction.y * speed * Time.deltaTime
+                    joystick.Vertical * speed * Time.deltaTime
                 );
-    }
 
+        Vector3 pos = transform.position + _speed;
+        pos.x = Mathf.Clamp(pos.x + _speed.x, -screenBounds.x/7 + objectWidth, screenBounds.x/7 - objectWidth);
+        pos.z = Mathf.Clamp(pos.z + _speed.z, screenBounds.y/10 + objectHeight, -screenBounds.y/10 - objectHeight);
+        transform.position = pos;
+    }
+    void ControlEngineFlameLength()
+    {
+        foreach (ParticleSystem engine in engines)
+        {
+            Vector3 engineScale = engine.transform.localScale;
+            engineScale.z = joystick.Vertical*speed + 20;
+            engine.transform.localScale = engineScale;
+        }
+    }
     private void Roll()
     {
-        Vector3 rot = transform.rotation.eulerAngles;
-        rot.z = joystick.Direction.x * -tilt * Time.deltaTime;
-        transform.rotation = Quaternion.Euler(rot);
+        _tilt = transform.rotation.eulerAngles;
+        _tilt.z = joystick.Direction.x * -tilt * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(_tilt);
+
+    }
+
+    private void EaseOut()
+    {
+        if (_tilt.sqrMagnitude > 0f)
+        {
+            _tilt *= tiltEase;
+            _speed *= speedEase;
+            transform.rotation = Quaternion.Euler(_tilt);
+            transform.position += _speed;
+            if (_tilt.sqrMagnitude < 0.01f * 0.01f) _tilt = Vector3.zero;
+        }
     }
 
     private void Shoot(bool shoot)
     {
         if (shoot)
         {
-            shots.Play();
+            foreach (ParticleSystem shot in shots)
+            {
+                shot.Play();
+            }
         }
         else
         {
-            shots.Stop();
+            foreach (ParticleSystem shot in shots)
+            {
+                shot.Stop();
+            }
         }
     }
 }
